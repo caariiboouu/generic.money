45bd8d1140e83d99e1caeda54d0b218563337360
Joel Cuthriell <joel@cuthriell.com>
2022-04-12 04:02:55 +0000

    functioning insert

---
+++ screens/TabOneScreen.tsx
@@ -0,0 +1,71 @@
+import { StyleSheet } from 'react-native';
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+import { styleReset, List, ListItem, Divider } from 'react95';
+// pick a theme of your choice
+import original from "react95/dist/themes/original";
+// original Windows95 font (optionally)
+import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
+import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body {
+    font-family: 'ms_sans_serif';
+  }
+  ${styleReset}
+`;
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+        <div>
+          <GlobalStyles />
+          <ThemeProvider theme={original}>
+            <List>
+              <ListItem>🎤 Sing</ListItem>
+              <ListItem>💃🏻 Dance</ListItem>
+              <Divider />
+              <ListItem disabled>😴 Sleep</ListItem>
+            </List>
+          </ThemeProvider>
+        </div>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
